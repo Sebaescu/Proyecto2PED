d5600762563ec737572ed0bf4a1c65e98faa1abb
Alex-Benites <136673432+Alex-Benites@users.noreply.github.com>
2023-12-28 00:41:06 -0500

    Delete src/main/java directory

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.sebaescu.proyecto2ped {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.sebaescu.proyecto2ped to javafx.fxml;
-    exports com.sebaescu.proyecto2ped;
-}
