2cf48692e2de7632fc378ff8d30b674d773eb393
Alex-Benites <136673432+Alex-Benites@users.noreply.github.com>
2024-01-14 21:28:43 -0500

    Imagen de Logo App puesta

--- src/main/java/Controllers/Main.java
+++ src/main/java/Controllers/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -14,12 +15,11 @@ public class Main extends Application {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("MenuPrincipal.fxml"));
         Parent root = loader.load();
         
-        // Obtener el controlador del Main.fxml
         MenuPrincipalController menuController = loader.getController();
         menuController.setPrimaryStage(primaryStage);
-
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Tic-Tac-Tou");
         primaryStage.setScene(new Scene(root));
+        primaryStage.getIcons().add(new Image("file:Logo/Imagen2.jpg"));        
         primaryStage.show();
     }
 
@@ -28,18 +28,3 @@ public class Main extends Application {
     }
 }
 
-//public class Main extends Application {
-//
-//    @Override
-//    public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("VsComputador.fxml"));
-//        primaryStage.setTitle("Hello World");
-//        primaryStage.setScene(new Scene(root));
-//        primaryStage.show();
-//    }
-//
-//
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
-//}
